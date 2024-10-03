785e963cd06eccac6e2060bf6e5623554d78a19c
ou mai goss <haison27102003@gmail.com>
2024-10-03 11:30:42 +0700

    Update Form1.cs
    sua code master

--- Maytinhkiemtra/Form1.cs
+++ Maytinhkiemtra/Form1.cs
@@ -20,70 +20,7 @@ namespace Maytinhkiemtra
         private void Form1_Load(object sender, EventArgs e)
         {
         }
-
-        private void Calculate(string operation)
-        {
-            double number1, number2;
-
-            // Kiểm tra và chuyển đổi giá trị đầu vào
-            if (double.TryParse(txtso1.Text, out number1) && double.TryParse(txtso2.Text, out number2))
-            {
-                double result = 0;
-
-                // Thực hiện phép toán dựa trên tham số operation
-                switch (operation)
-                {
-                    case "+":
-                        result = number1 + number2;
-                        break;
-                    case "-":
-                        result = number1 - number2;
-                        break;
-                    case "*":
-                        result = number1 * number2;
-                        break;
-                    case "/":
-                        if (number2 != 0)
-                            result = number1 / number2;
-                        else
-                            MessageBox.Show("Không thể chia cho 0!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        break;
-                }
-
-                txtKQ.Text = result.ToString();
-            }
-            else
-            {
-                MessageBox.Show("Vui lòng nhập số hợp lệ!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void btnCong_Click(object sender, EventArgs e)
-        {
-            Calculate("+");
-        }
-
-        private void btnTru_Click(object sender, EventArgs e)
-        {
-            Calculate("-");
-        }
-
-        private void btnNhan_Click(object sender, EventArgs e)
-        {
-            Calculate("*");
-        }
-
-        private void btnChia_Click(object sender, EventArgs e)
-        {
-            Calculate("/");
-        }
-    }
-     private void btnTru_Click(object sender, EventArgs e)
-        {
-            Calculate("-");
-        }
-
-        private void btnSubtract_Click(object sender, EventArgs e)
+      private void btnTru_Click(object sender, EventArgs e)
         {
             double number1, number2;
             if (double.TryParse(txtNumber1.Text, out number1) && double.TryParse(txtNumber2.Text, out number2))
